f000b494610e96539b03d10693824110e65765da
Stuart Sierra <mail@stuartsierra.com>
2009-07-23 11:55:55 -0400

    dcterms.clj: added remaining classes & types

--- src/clojure_rdf/vocabs/dcterms.clj
+++ src/clojure_rdf/vocabs/dcterms.clj
@@ -4,6 +4,7 @@
 
 (use-prefix "dcterms" "http://purl.org/dc/terms/")
 
+;; Properties
 (defresources abstract, accessRights, accrualMethod,
 accrualPeriodicity, accrualPolicy, alternative, audience, available,
 bibliographicCitation, conformsTo, contributor, coverage, created,
@@ -15,3 +16,18 @@ language, license, mediator, medium, modified, provenance, publisher,
 references, relation, replaces, requires, rights, rightsHolder,
 source, spatial, subject, tableOfContents, temporal, title, type,
 valid)
+
+;; Vocabulary encoding schemes
+(defresources DCMIType, DDC, IMT, LCC, LCSH, MESH, NLM, TGN, UDC)
+
+;; Syntax encoding schemes
+(defresources Box, ISO3166, ISO639-2, ISO639-3, Period, Point,
+RFC1766, RFC3066, RFC4646, URI, W3CDTF)
+
+;; Classes
+(defresources Agent, AgentClass, BibliographicResource, FileFormat,
+Frequency, Jurisdiction, LicenseDocument, LinguisticSystem, Location,
+LocationPeriodOrJurisdiction, MediaType, MediaTypeOrExtent,
+MethodOfAccrual, MethodOfInstruction, PeriodOfTime, PhysicalMedium,
+PhysicalResource, Policy, ProvenanceStatement, RightsStatement,
+SizeOrDuration, Standard)
