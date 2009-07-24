cdf5227dde333b159e9ccc8ca1383140475d875d
Stuart Sierra <mail@stuartsierra.com>
2009-07-24 16:20:42 -0400

    rio.clj: added doc about Sesame 1.x jars, added writing fns

--- src/clojure_rdf/rio.clj
+++ src/clojure_rdf/rio.clj
@@ -1,11 +1,19 @@
-(ns clojure-rdf.rio
+(ns #^{:doc "Input/Output in various RDF formats using the Rio
+    libraries from openrdf.org.  
+
+    This lib requires the Sesame 1.x JARs on the Java classpath.
+    Sesame 2.x will not work!  The JARs from the Sesame 1.x
+    distribution are named openrdf-model.jar, openrdf-util.jar,
+    rio.jar, and sesame.jar."}
+  clojure-rdf.rio
   (:require [clojure.contrib.duck-streams :as duck])
   (:use clojure-rdf.model)
   (:import (org.openrdf.rio StatementHandler)
            (org.openrdf.model URI BNode Literal)
-           (org.openrdf.rio.ntriples NTriplesParser)
-           (org.openrdf.rio.rdfxml RdfXmlParser)
-           (org.openrdf.rio.turtle TurtleParser)))
+           (org.openrdf.model.impl URIImpl LiteralImpl BNodeImpl)
+           (org.openrdf.rio.ntriples NTriplesParser NTriplesWriter)
+           (org.openrdf.rio.rdfxml RdfXmlParser RdfXmlWriter)
+           (org.openrdf.rio.turtle TurtleParser TurtleWriter)))
 
 (defmulti from-rio class)
 
@@ -19,6 +27,19 @@
   (make-literal (.getLabel x) (when (.getDatatype x)
                                 (.getURI (.getDatatype x)))
                 (.getLanguage x)))
+
+(defmulti to-rio :type)
+
+(defmethod to-rio :resource [r]
+  (URIImpl. (:uri r)))
+
+(defmethod to-rio :blank-node [b]
+  (BNodeImpl. (str (:id b))))
+
+(defmethod to-rio :literal [lit]
+  (cond (:datatype lit) (LiteralImpl. (:value lit) (URIImpl. (:datatype lit)))
+        (:language lit) (LiteralImpl. (:value lit) (:language lit))
+        :else (LiteralImpl. (:value lit))))
 
 (defn statement-handler [graph-atom]
   (proxy [StatementHandler] []
@@ -49,3 +70,19 @@
   ([input base]
      (parse-with (TurtleParser.) input base)))
 
+(defn- write-with [writer graph]
+  (.startDocument writer)
+  (doseq [stmt (:stmts graph)]
+    (.writeStatement writer (to-rio (:subj stmt))
+                     (to-rio (:pred stmt))
+                     (to-rio (:obj stmt))))
+  (.endDocument writer))
+
+(defn write-ntriples [output graph]
+  (write-with (NTriplesWriter. (duck/writer output)) graph))
+
+(defn write-rdfxml [output graph]
+  (write-with (RdfXmlWriter. (duck/writer output)) graph))
+
+(defn write-turtle [output graph]
+  (write-with (TurtleWriter. (duck/writer output)) graph))
