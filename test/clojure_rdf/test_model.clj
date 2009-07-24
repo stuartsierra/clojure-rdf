b8da7b51280a31b594572ca721e8ffc6567d995d
Stuart Sierra <mail@stuartsierra.com>
2009-07-24 14:54:46 -0400

    test_model.clj: added first simple tests

---
+++ test/clojure_rdf/test_model.clj
@@ -0,0 +1,34 @@
+(ns clojure-rdf.test-model
+  (:use clojure.test clojure-rdf.model))
+
+(def *alan* "http://dbpedia.org/resource/Alan_Turing")
+
+(def *string* "http://www.w3.org/2001/XMLSchema#string")
+
+(def *almaMater* "http://dbpedia.org/property/almaMater")
+
+(def *princeton* "http://dbpedia.org/resource/Princeton_University")
+
+(deftest t-make-resource
+  (is (resource? (make-resource *alan*))))
+
+(deftest t-make-blank-node
+  (is (blank-node? (make-blank-node)))
+  (is (blank-node? (make-blank-node "bnode101"))))
+
+(deftest t-make-literal
+  (is (literal? (make-literal "Hello, World!")))
+  (is (literal? (make-literal "Hello, World!" *string*)))
+  (is (literal? (make-literal "Hello, World!" *string* "en"))))
+
+(deftest t-make-stmt
+  (is (stmt? (make-stmt (make-resource *alan*)
+                        (make-resource *almaMater*)
+                        (make-resource *princeton*)))))
+
+(deftest t-make-graph
+  (is (graph? (make-graph)))
+  (is (graph? (make-graph #{})))
+  (is (graph? (make-graph #{(make-stmt (make-resource *alan*)
+                                       (make-resource *almaMater*)
+                                       (make-resource *princeton*))}))))
